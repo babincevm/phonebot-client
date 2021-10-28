db437e2558893056a7ab8895b1dfec1d1e6800e4
babincevm <babincevm@icloud.com>
2021-10-28 20:23:31 +0500

    style prop directive

--- src/main.js
+++ src/main.js
@@ -7,15 +7,19 @@ import helpers from "./helpers";
 
 Vue.config.productionTip = false;
 helpers.setPolyfills();
-let y = {
-  a: {
-    b: "b",
-    c: "c",
-  },
-  d: "d",
-};
-y.flatten();
-console.log(y);
+Vue.directive("style-props", function (el, { value }) {
+  for (let property in value) {
+    if (!Object.prototype.hasOwnProperty.call(value, property)) {
+      continue;
+    }
+    if (value[property] === null) {
+      el.style.removeProperty(property);
+      continue;
+    }
+
+    el.style.setProperty(`--${property}`, value[property]);
+  }
+});
 
 new Vue({
   router,
